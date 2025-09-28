a9721a009ef7a82d479624f13a118d05c591862e
zach <zachmonari002@gmail.com>
2025-09-28 12:51:25 +0300

    added two fuctions

--- Expenses.py
+++ Expenses.py
@@ -1,11 +1,15 @@
-while True:
-    item=input("Enter the item bought ('quit' to stop): ")
-    if item.lower() == "quit":
-        break
-    amount=input("Enter the amount spent on the item: ")
-    with open("Expenses.txt", "a") as file:
-        file.write(item+": Ksh" +amount+"\n")
-
-with open("Expenses.txt", "r") as file:
-    print("Here are all your expenses saved!")
-    print(file.read())
+def log_expenses():
+    while True:
+        item = input("Enter the item bought ('quit' to stop): ")
+        if item.lower() == "quit":
+            break
+        amount = input("Enter the amount spent on the item: ")
+        with open("Expenses.txt", "a") as file:
+            file.write(item + ": Ksh" + amount + "\n")
+    print("All expenses have been logged.")
+def view_expenses():
+    with open("Expenses.txt", "r") as file:
+        print("Here are all your expenses saved!")
+        print(file.read())
+log_expenses()
+view_expenses()
